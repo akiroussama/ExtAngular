dc607601a9ce66cc0f0819b8ab3d60029fa02749
Akir Oussama <akir.oussama@gmail.com>
2019-03-16 23:46:50 +0100

    First commit

---
+++ build/ext-angular/temp/App-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag App
+// @require D:\workspace\extAngular\cra-ext-ang\build\ext-angular\app.js
---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+declare var Ext: any;
+import { Component, VERSION } from '@angular/core'
+import { Router } from '@angular/router'
+import { data } from './Data'
+
+Ext.require([
+  'Ext.layout.*',
+//  'Ext.MessageBox',
+//  'Ext.Toast',
+//  'Ext.panel.Collapser',
+  'Ext.data.TreeStore'
+])
+
+@Component({
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styles: [``]
+})
+export class AppComponent {
+  isPhone = Ext.platformTags.phone;
+  top = !this.isPhone ? '10' : null
+  left = !this.isPhone ? '10' : null
+  width = !this.isPhone ? '400' : null
+  height = !this.isPhone ? '600' : null
+  title = 'Sencha ExtAngular 6.7 Boilerplate - Angular v' + VERSION.full
+
+  constructor(private router: Router) {}
+
+  showAppMenu: boolean = false;
+  toggleAppMenu = () => {
+    this.showAppMenu = !this.showAppMenu
+  }
+
+  onHideAppMenu = () => {
+    this.showAppMenu = false
+  }
+
+  navigate = (event) => {
+    var record = event.record;
+    this.router.navigate([record.data.id])
+  }
+
+  store = Ext.create('Ext.data.TreeStore', {
+    rootVisible: true,
+    root: data
+  });
+
+  responsiveConfig = {
+    medium: {
+      micro: true,
+      width: 48
+    },
+    large: {
+      micro: false,
+      width: 200
+    }
+  }
+  nav = true;
+  micro = false;
+  
+  toggleMicro = function(event) {
+    this.micro = event.value;
+    this.nav = this.micro || this.nav;
+    this.width = this.micro ? 56 : undefined; 
+  }
+
+}
